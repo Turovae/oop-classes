bfde1e103b8dcb875346040c9c32302d5f03b5f8
Turovae <ekaterina.turova@mail.ru>
2022-12-24 15:58:24 +0700

    change classes

--- src/characters/__tests__/magician.js
+++ src/characters/__tests__/magician.js
@@ -1,28 +1,35 @@
 import Magician from '../magician';
 
-const magician = {
-  name: 'magician1',
-  type: 'Magician',
-  health: 100,
-  level: 1,
-  attack: 10,
-  defence: 40,
-};
+// const magician = {
+//   name: 'magician1',
+//   type: 'Magician',
+//   health: 100,
+//   level: 1,
+//   attack: 10,
+//   defence: 40,
+// };
 
 test('test correct magician', () => {
-  const result = new Magician('magician1');
-  expect(result).toEqual(magician);
+  // const result = new Magician('magician1');
+  expect(new Magician('magician')).toEqual({
+    name: 'magician',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  });
 });
 
-const incorrectNameMagicians = [
-  ['to short name', 'a'],
-  ['to long name', '123456789ab'],
-];
-test.each(incorrectNameMagicians)(
-  ('testing creating Magicians instance with %s'),
-  (_, name) => {
-    expect(() => {
-      new Magician(name);
-    }).toThrow('Длинна имени должна быть от 2 до 10 символов');
-  },
-);
+// const incorrectNameMagicians = [
+//   ['to short name', 'a'],
+//   ['to long name', '123456789ab'],
+// ];
+// test.each(incorrectNameMagicians)(
+//   ('testing creating Magicians instance with %s'),
+//   (_, name) => {
+//     expect(() => {
+//       new Magician(name);
+//     }).toThrow('Длинна имени должна быть от 2 до 10 символов');
+//   },
+// );
